0988f555dfe70df5669664e6ff691b6bc4aea305
sanjusathwik <sanjusathiwk777@gmail.com>
2026-03-07 10:12:18 +0530

    docker-installation

---
+++ docker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+ ARCH=amd64
+ PLATFORM=$(uname -s)_$ARCH 
+#resize-dis
+
+growpart /dev/nvme0n1 4
+lvextend -l +50%FREE /dev/RootVG/rootVol
+lvextend -l +50%FREE /dev/RootVG/varVol
+xfs_growfs /
+xfs_growfs /var
+
+#installing docker
+
+dnf -y install dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo 
+dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+systemctl start docker
+systemctl enable docker
+systemctl moduser -aG docker ec2-user
+
+#kubectl installation
+
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.32.12/2026-02-27/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv kubectl /usr/local/bin/kubectl
+
+#eksctl
+curl -sLO "http://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+mv /tmp/eksctl /urs/local/bin
